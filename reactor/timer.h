e380cfb2372e37d70aecb4602e47498a4bbaf040
zhangzewen <daixiuhua_1989@163.com>
2016-01-19 14:03:45 -0500

    modify

--- reactor/timer.h
+++ reactor/timer.h
@@ -1,51 +1,86 @@
 #ifndef REACTOR_TIMER_H_
 #define REACTOR_TIMER_H_
 
+#include <sys/time.h>
+
+#ifndef NULL
+#define NULL 0
+#endif
+
 class Timer
 {
 public:
     Timer& operator+(const Timer& time) {
-        this.time_.tv_sec = this.time_.tv_sec + time.time_.tv_sec;
-        this.time_.tv_usec = this.time_.tv_usec + time.time_.tv_usec;
+        this->time_.tv_sec = this->time_.tv_sec + time.time_.tv_sec;
+        this->time_.tv_usec = this->time_.tv_usec + time.time_.tv_usec;
 
-        if (this.time_.tv_usec >= 1000000) {
-            this.time_.tv_usec -= 1000000;
-            this.time_.tv_sec += 1;
+        if (this->time_.tv_usec >= 1000000) {
+            this->time_.tv_usec -= 1000000;
+            this->time_.tv_sec += 1;
         }
         return *this;
     }
 
     Timer& operator-(const Timer& time) {
-        this.time_.tv_sec = this.time_.tv_sec - time.time_.tv_sec;
-        this.time_.tv_usec = this.time_.tv_usec - time.time_.tv_usec;
+        this->time_.tv_sec = this->time_.tv_sec - time.time_.tv_sec;
+        this->time_.tv_usec = this->time_.tv_usec - time.time_.tv_usec;
 
-        if (this.time_.tv_usec < 0) {
-            this.time_.tv_usec += 1000000;
-            this.time_.tv_sec -= 1;
+        if (this->time_.tv_usec < 0) {
+            this->time_.tv_usec += 1000000;
+            this->time_.tv_sec -= 1;
         }
         return *this;
     }
 
     bool operator<(const Timer& time) {
+        if (this->time_.tv_sec > time.time_.tv_sec) {
+            return true;
+        }
+        if (this->time_.tv_usec > time.time_.tv_usec) {
+            return true;
+        }
         return true;
     }
 
     bool operator>(const Timer& time) {
+        if (this->time_.tv_sec < time.time_.tv_sec) {
+            return true;
+        }
+        if (this->time_.tv_usec < time.time_.tv_usec) {
+            return true;
+        }
         return true;
     }
+
+    bool operator==(const Timer& time) {
+        return (this->time_.tv_sec == time.time_.tv_sec
+            && this->time_.tv_usec == time.time_.tv_usec);
+    }
+
     void now() {
         struct timeval current_time;
         timer_reset(&current_time);
         gettimeofday(&current_time, NULL);
         time_ = current_time;
+    }
 
+    void timer_reset() {
+        this->time_.tv_sec = 0;
+        this->time_.tv_usec = 0;
+    }
+
+    void timer_reset(Timer& time) {
+        time.time_.tv_sec = 0;
+        time.time_.tv_usec = 0;
+    }
+
+    void timer_reset(struct timeval* time) {
+        time->tv_sec = 0;
+        time->tv_usec = 0;
     }
 
 private:
     struct timeval time_;
-    int day_;
-    int month_;
-    int year_;
 };
 
 #endif
