fd19fbdff0d090077aa1d398d7c49a141393e3d9
zhangzewen <daixiuhua_1989@163.com>
2016-04-26 15:04:49 -0400

    modify

--- buffer/FileBuffer.h
+++ buffer/FileBuffer.h
@@ -7,11 +7,14 @@
 class FileBuffer : public BufferDecorater
 {
 public:
+    FileBuffer(Buffer* buf);
+    FileBuffer(Buffer* buf, const std::string& path);
     int read(int fd, char* buf, int len);
     int write(int fd, char* buf, int len);
+    bool open();
+    void close();
 private:
     int fd_;
-    File* fp_;
     std::string path_;
     Buffer* buffer_;
 
